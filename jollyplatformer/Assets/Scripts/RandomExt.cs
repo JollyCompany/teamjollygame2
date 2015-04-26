0d60b8c636f74f5fc389eeb706e650336bd2f35b
Vijay Thakkar <nozomiyume@gmail.com>
2015-04-25 20:42:48 -0500

    Random

---
+++ jollyplatformer/Assets/Scripts/RandomExt.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Jolly
+{
+	public class RandomExt
+	{
+		public static T Pick<T>(T[] array)
+		{
+			if (array.Length == 0)
+			{
+				return default(T);
+			}
+			return array[Random.Range(0, array.Length-1)];
+		}
+
+		public static T Pick<T>(List<T> list)
+		{
+			if (list.Count == 0)
+			{
+				return default(T);
+			}
+			return list[Random.Range(0, list.Count-1)];
+		}
+
+		public static void Shuffle<T>(ref T[] array)
+		{
+			for (int i = array.Length - 1; i > 0; --i)
+			{
+				int j = RandomExt.RangeExclusive (0, i);
+				T swap = array[i];
+				array[i] = array[j];
+				array[j] = swap;
+			}
+		}
+
+		public static int RangeInclusive(int min, int max)
+		{
+			JollyDebug.Assert (min <= max, "Random.RangeInclusive({0},{1}) - Invalid range", min, max);
+			return Random.Range (min, max);
+		}
+
+		public static int RangeExclusive(int min, int max)
+		{
+			JollyDebug.Assert (min < max, "Random.RangeExclusive({0},{1}) - Invalid range", min, max);
+			return Random.Range (min, max-1);
+		}
+
+		public static float RangeInclusive(float min, float max)
+		{
+			JollyDebug.Assert (min <= max, "Random.RangeInclusive({0},{1}) - Invalid range", min, max);
+			return Random.Range (min, max);
+		}
+	}
+}
